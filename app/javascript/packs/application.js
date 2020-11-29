d1eff68f3a525c161fa17e96d1034795141f2031
Maxim Zelenkin <nudepatch@gmail.com>
2020-11-29 10:52:41 +0500

    Migrate owl.carousel

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -13,5 +13,8 @@ require('jquery')
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-import "bootstrap"
 import "scss/application"
+
+import "bootstrap"
+
+import "owl.carousel"
